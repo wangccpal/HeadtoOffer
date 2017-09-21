79c423a44fcb7893d31e56e99ef91e118fad4242
CC <CC@LAPTOP-747MPDHJ>
2017-09-21 10:22:42 +0800

    复习了一下d算法，重新捋了一遍构造方法静态方法构造快的顺序

--- src/javapuzzle/BlockAndStaticBlock.java
+++ src/javapuzzle/BlockAndStaticBlock.java
@@ -1,5 +1,10 @@
 package javapuzzle;
-
+/**
+ * 注意静态变量的初始化在静态块的前面
+ * 即一个类的加载+创建对象的顺序是  静态变量-》静态块-》构造函数-》构造快
+ * @author CC
+ *
+ */
 public class BlockAndStaticBlock {
 	public static BlockAndStaticBlock test = new BlockAndStaticBlock();
 	{
@@ -11,7 +16,6 @@ public class BlockAndStaticBlock {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new BlockAndStaticBlock();
 		new BlockAndStaticBlock();
 		
 	}
