a9571e4852e4f9da2f68f1fa354e6d4b5fd027ae
wangchenge <wangchenge@yidian-inc.com>
2019-11-08 19:16:20 +0800

    complete 132

--- src/leetcode/LT132.java
+++ src/leetcode/LT132.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * minCut
  *
@@ -8,25 +10,41 @@ package leetcode;
  * @see L131PalindromePart
  **/
 public class LT132 {
+    boolean [][] dp;
+    int [][] dp2;
     public static void main(String[] args) {
-        System.out.println(new LT132().minCut("eegiicgaeadbcfacfhifdbiehbgejcaeggcgbahfcajfhjjdgj"));
+        System.out.println(new LT132().minCut("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
     }
 
     public int minCut(String s) {
+        dp = new boolean[s.length()][s.length()];
+        dp2 = new int[s.length()][s.length()];
+        for (int[] as : dp2) {
+            Arrays.fill(as, -1);
+        }
+        for (int len = 1; len <= s.length(); len++) {
+            for (int i = 0; i <= s.length() - len; i++) {
+                //dp[i][j] 对应 s.substring(i, j+1)
+                if (isPalindrome(s.substring(i, i + len))) dp[i][i + len - 1] = true;
+            }
+        }
         return process(s, 0, s.length());
     }
 
     private int process(String s, int start, int end) {
+        if (dp2[start][end-1] != -1) return dp2[start][end-1];
         int min = Integer.MAX_VALUE;
-        if (isPalindrome(s.substring(start, end))) {
+        if (dp[start][end-1]) {
             return 0;
         } else {
             for (int i = 1; i < end - start; i++) {
-                int cut = 0;
-                cut = 1 + process(s, start, start + i) + process(s, start + i, end);
-                if (cut < min) min = cut;
+                if (dp[start][start+i-1]){
+                    int cut = 1 + process(s, start, start + i) + process(s, start + i, end);
+                    if (cut < min) min = cut;
+                }
             }
         }
+        dp2[start][end-1] = min;
         return min;
     }
 
